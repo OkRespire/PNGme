93f34af9419e37e4e9dfafafcd2ea1cf4435aa67
OkRespire <rohane04@gmail.com>
2025-06-09 13:12:03 +0100

    Made main backend into a library and allowed my tauri app to point to it

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use crate::args::{Args, PngMeArgs};
+
+pub mod args;
+pub mod chunk;
+pub mod chunk_types;
+pub mod commands;
+pub mod png;
+
+pub use commands::Commands;
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+pub fn decode_with_args(args: crate::args::DecodeArgs) -> Result<String> {
+    let runner = Commands::new();
+    runner.decode(args)
+}
+
+pub fn encode_with_args(args: crate::args::EncodeArgs) -> Result<String> {
+    let runner = Commands::new();
+    runner.encode(args)
+}
+
+pub fn remove_with_args(args: crate::args::RemoveArgs) -> Result<String> {
+    let runner = Commands::new();
+    runner.remove(args)
+}
+
+pub fn print_with_args(args: crate::args::PrintArgs) -> Result<String> {
+    let runner = Commands::new();
+    runner.print(args)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 use clap::Parser;
-mod args;
-mod chunk;
-mod chunk_types;
-mod commands;
-mod png;
-use args::{Args, PngMeArgs};
-use commands::Commands;
-
-pub type Error = Box<dyn std::error::Error>;
-pub type Result<T> = std::result::Result<T, Error>;
+use pngme::{
+    Commands, Result,
+    args::{Args, PngMeArgs},
+};
 
 fn main() -> Result<()> {
     let args = Args::parse();
